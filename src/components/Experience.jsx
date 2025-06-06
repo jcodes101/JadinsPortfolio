d7bc87bdcc898576fe1c3f88017c2f164ae03e96
jadin <jshutchinson@aggies.ncat.edu>
2025-06-06 04:44:24 -0400

    experience tech format fixed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -23,11 +23,13 @@ const Experience = () => {
                             </h6>
 
                             <p className="mb-4 font-thin">{experience.description}</p>
-                            {experience.technologies.map((tech, index) => (
-                                <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
-                                    {tech}
-                                </span>
-                            ))}
+                            <div className="flex flex-wrap">
+                                {experience.technologies.map((tech, index) => (
+                                    <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
 
                     </div>
